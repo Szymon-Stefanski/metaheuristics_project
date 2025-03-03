044bc338d850fd6eb9dafc8678437e10d8774587
Szymon Stefański <szymonstefanski1@gmail.com>
2025-03-03 21:08:40 +0100

    Update project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Knapsack problem (pol. problem plecakowy) - problem optymalizacyjny dotyczący optymalnego wyboru 
 przedmiotów, tak by ich wartość całkowita była jak największa, a jednocześnie by dało się je
-pomieścić w plecaków.
+pomieścić w plecaku.
 
--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 
 goods = [{ "name" : "guitar", "price" : 500, "weight" : 4},
-         { "name" : "violin", "price" : 900, "weight" : 1},
+         { "name" : "violin", "price" : 900, "weight" : 2},
          { "name" : "monitor", "price" : 1000, "weight" : 3},
          { "name" : "laptop", "price" : 1500, "weight" : 4},
          { "name" : "watch", "price" : 700, "weight" : 1},
-         { "name" : "phone", "price" : 600, "weight" : 2},
+         { "name" : "phone", "price" : 600, "weight" : 1},
          ]
 
-max = 4
-
-permuts = []
 
 knapsack = []
 
-def goal(arr, limit):
-    for i in range(len(arr)):
-        for j in range(len(arr)-1):
-            if (arr[i].get("name") != arr[j+1].get("name")) and (arr[i].get("weight") + arr[j+1].get("weight")) <= limit:
-                permuts.append({"name":arr[i].get("name") + " , " + arr[j+1].get("name"),
-                                "price":arr[i].get("price") + arr[j+1].get("price"),
-                                "weight":arr[i].get("weight") + arr[j+1].get("weight")})
+permutations = []
 
-    return permuts
+def goal(arr, limit):
+        for i in range(len(arr)):
+            if arr[i].get("weight") == limit:
+                permutations.append({"name": arr[i].get("name"),
+                                 "price": arr[i].get("price"),
+                                 "weight": arr[i].get("weight")})
+            for j in range(len(arr)):
+                if (arr[i].get("name") != arr[j].get("name")) and (arr[i].get("weight") + arr[j].get("weight")) <= limit:
+                    permutations.append({"name": arr[i].get("name") + arr[j].get("name"),
+                                    "price" : arr[i].get("price") + arr[j].get("price"),
+                                    "weight" : arr[i].get("weight") + arr[j].get("weight")})
+
+        for i in range(len(permutations)):
+            if permutations[i].get("weight") < limit:
+                knapsack.append({"name": permutations[i].get("name"),
+                                 "price": permutations[i].get("price"),
+                                 "weight": permutations[i].get("weight")})
+
+        return knapsack
 
 
 def nearest_neighbour(arr):
@@ -33,6 +42,6 @@ def random_solution(arr):
     return 0
 
 
-print(goal(goods,max))
+print(goal(goods,5))
 #print(nearest_neighbour(goods))
 
