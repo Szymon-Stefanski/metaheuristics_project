027f8dd372d1d027635d121afa0ec89dda972e6b
Szymon Stefański <szymonstefanski1@gmail.com>
2025-03-04 22:35:47 +0100

    Update project - code refactor

--- main.py
+++ main.py
@@ -7,41 +7,34 @@ goods = [{ "name" : "guitar", "price" : 500, "weight" : 4},
          { "name" : "phone", "price" : 600, "weight" : 1},
          ]
 
-
 knapsack = []
 
-permutations = []
 
-def goal(arr, limit):
-        for i in range(len(arr)):
-            if arr[i].get("weight") == limit:
-                permutations.append({"name": arr[i].get("name"),
-                                 "price": arr[i].get("price"),
-                                 "weight": arr[i].get("weight")})
-            for j in range(len(arr)):
-                if (arr[i].get("name") != arr[j].get("name")) and (arr[i].get("weight") + arr[j].get("weight")) <= limit:
-                    permutations.append({"name": arr[i].get("name") + arr[j].get("name"),
-                                    "price" : arr[i].get("price") + arr[j].get("price"),
-                                    "weight" : arr[i].get("weight") + arr[j].get("weight")})
+def goal(arrays, limit):
+    for array in arrays:
+        if array.get("weight") == limit:
+            knapsack.append(array)
+        else:
+            for i in range(len(goods)):
+                for j in range(len(goods)):
+                    if i != j:
+                        if goods[i].get("weight") + goods[j].get("weight") <= limit:
+                            knapsack.append({"name" : goods[i].get("name") + " " + goods[j].get("name"),
+                                             "price" : goods[i].get("price") + goods[j].get("price"),
+                                             "weight" : goods[i].get("weight") + goods[j].get("weight")})
 
-        for i in range(len(permutations)):
-            if permutations[i].get("weight") < limit:
-                knapsack.append({"name": permutations[i].get("name"),
-                                 "price": permutations[i].get("price"),
-                                 "weight": permutations[i].get("weight")})
+    knapsack.sort(key=lambda x: x["price"], reverse=True)
 
-        return knapsack
+    return knapsack
 
 
 def nearest_neighbour(arr):
     return [arr[i].get("name") + "," + arr[i + 1].get("name") for i in range(len(arr) - 1)]
 
 
-
 def random_solution(arr):
     return 0
 
 
-print(goal(goods,5))
-#print(nearest_neighbour(goods))
-
+print(goal(goods,4))
+print(nearest_neighbour(goods))
