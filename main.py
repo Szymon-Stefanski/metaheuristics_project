3945727bd1212ba0e7a8b6f17bcb51b8544bda18
Szymon Stefański <szymonstefanski1@gmail.com>
2025-03-05 21:20:23 +0100

    Modify project - code refactor

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 
-goods = [{ "name" : "guitar", "price" : 500, "weight" : 4},
-         { "name" : "violin", "price" : 900, "weight" : 2},
-         { "name" : "monitor", "price" : 1000, "weight" : 3},
-         { "name" : "laptop", "price" : 1500, "weight" : 4},
-         { "name" : "watch", "price" : 700, "weight" : 1},
-         { "name" : "phone", "price" : 600, "weight" : 1},
-         ]
 
-knapsack = []
+def goal(arrays, target, limit):
+    for i in range(len(arrays)):
+        if arrays[i].get("weight") == limit:
+            target.append(arrays[i])
+            arrays.remove(arrays[i])
+            target.sort(key=lambda x: x["price"], reverse=True)
+        elif arrays[i].get("weight") < limit:
+            for j in range(len(arrays)):
+                while (arrays[j].get("weight") + arrays[i].get("weight")) <= limit:
+                    if (arrays[j].get("name") not in arrays[i].get("name")) and (arrays[i].get("name") not in arrays[j].get("name")):
+                        arrays.append({"name" : arrays[j].get("name") + " " + arrays[i].get("name"),
+                                      "price" : arrays[j].get("price") + arrays[i].get("price"),
+                                      "weight" : arrays[j].get("weight") + arrays[i].get("weight")})
+                    break
 
+    arrays.sort(key=lambda x: x["price"], reverse=True)
 
-def goal(arrays, limit):
-    for array in arrays:
-        if array.get("weight") == limit:
-            knapsack.append(array)
+    for n in range(len(arrays) - 1, -1, -1):
+        if arrays[n].get("weight") != limit:
+            del arrays[n]
         else:
-            for i in range(len(goods)):
-                for j in range(len(goods)):
-                    if i != j:
-                        if goods[i].get("weight") + goods[j].get("weight") <= limit:
-                            knapsack.append({"name" : goods[i].get("name") + " " + goods[j].get("name"),
-                                             "price" : goods[i].get("price") + goods[j].get("price"),
-                                             "weight" : goods[i].get("weight") + goods[j].get("weight")})
+            if arrays[n].get("name") in arrays[n].get("name"):
+                target.append({"name": arrays[n].get("name"),
+                               "price": arrays[n].get("price"),
+                               "weight": arrays[n].get("weight")})
 
-    knapsack.sort(key=lambda x: x["price"], reverse=True)
-
-    return knapsack
+    target.sort(key=lambda x: x["price"], reverse=True)
+    return target
 
 
 def nearest_neighbour(arr):
@@ -36,5 +38,16 @@ def random_solution(arr):
     return 0
 
 
-print(goal(goods,4))
-print(nearest_neighbour(goods))
+goods = [{ "name" : "guitar", "price" : 500, "weight" : 4},
+         { "name" : "violin", "price" : 900, "weight" : 2},
+         { "name" : "monitor", "price" : 1000, "weight" : 3},
+         { "name" : "laptop", "price" : 1500, "weight" : 4},
+         { "name" : "watch", "price" : 700, "weight" : 1},
+         { "name" : "phone", "price" : 600, "weight" : 1},
+         ]
+
+
+knapsack = []
+
+print(goal(goods, knapsack,3))
+#print(nearest_neighbour(goods))
